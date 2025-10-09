fd686514234725ad8fcf657b80a6bb55061ae1ec
sawhardoSikto <143166338+sawhardoSikto@users.noreply.github.com>
2025-10-09 16:45:14 +0600

    some problem added

---
+++ Celsius_to_Fahrenheit.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+     
+int main() {
+    /**
+    * Write your C++ code from here
+    */
+   
+
+   float c,f;
+   cin>>c;
+   f=(c*9/5)+32;
+     cout << fixed << setprecision(2);
+   cout<<"The temperature in Fahrenheit is:" <<f;
+
+    return 0;
+}
---
+++ Triangle.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+     
+int main() {
+    /**
+    * Write your C++ code from here
+    */
+   int a,b ,result;
+   cin >>a>>b;
+   result=.5*a*b;
+   cout<<result;
+
+    return 0;
+}
